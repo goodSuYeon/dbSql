d096ccf4a3e5ec76ed65b4fe39c7d6804a2bf770
suyeon Kim <dutlstorgl@naver.com>
2020-09-23 18:17:34 +0900

    no message

--- 20200923.sql
+++ 20200923.sql
@@ -63,10 +63,29 @@ WHERE empno = :empno;
 
 --PPT 113 과제
 -- INDEX [실습 idx4]
--- 테이블  
-(1) emp , empno(=)
-(2) emp , deptno(=) , sal(BETWEEN)
-(3) deptno(=) , 
+(1) emp , empno(=)                                   -(1,2) 
+(2) emp, deptno(=) , empno(LIKE : empno || ' %')     -(1,2) 
+    dept, deptno(=)
+(3) dept, deptno(=)                                  -(3,5)
+(4) emp , deptno(=) , sal(BETWEEN)                   -(4)
+(5) dept, deptno(=) , loc(=)                         -(3,5)
+    emp , deptno(=)
+
+(1,2) CREATE UNIQUE INDEX idx_emp_u_001 ON emp(empno,deptno);
+(4) CREATE INDEX idx_emp_n_001 ON emp(deptno, sal);
+(3,5) CREATE UNIQUE INDEX idx_dept_u_002 ON dept(deptno, loc);
+
+
+EXPLAIN PLAN FOR
+SELECT *
+FROM emp, dept
+WHERE emp.deptno = dept.deptno
+AND emp.deptno = :deptno
+AND dept.loc = :loc;
+
+SELECT * FROM TABLE(dbms_xplan.display);
+
+SELECT * FROM dept;
 
 
 --synonym : 동의어
